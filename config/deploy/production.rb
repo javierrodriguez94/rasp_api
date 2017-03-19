b53d0b82bdf676039c9995884719f456de187ca8
javierrodriguez94 <javier.ri94@outlook.com>
2017-03-19 23:04:44 +0100

    try gpio on raspberry

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@ role :app, %w{192.168.1.179}
 role :web, %w{192.168.1.179}
 role :db,  %w{192.168.1.179}
 server '192.168.1.179', user: 'pi', roles: %w{web app}
+set :branch, :master
 set :ssh_options, {
     keys: %w(~/.ssh/id_rsa),
     forward_agent: false,
