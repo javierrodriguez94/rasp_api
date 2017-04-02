cbbc30ec009d0be610fa42747c7a560ac99e4eb2
javierrodriguez94 <javier.ri94@outlook.com>
2017-04-02 13:47:05 +0200

    Modularized solution

---
+++ app/controllers/camera_controller.rb
@@ -0,0 +1,11 @@
+class CameraController < ApplicationController
+
+  def photo
+    system "fswebcam -p #{ENV["CAMERA_PALETE"]}
+                     -r #{ENV["CAMERA_RESOLUTION"]}
+                     --no-banner
+                     #{ENV["CAMERA_DIR"]}"
+    send_file ENV["CAMERA_DIR"], type: 'image/jpg', disposition: 'inline'
+  end
+
+end
