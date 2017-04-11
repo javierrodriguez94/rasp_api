673950759e059df2b018847e7462ce48bb531401
javierrodriguez94 <javier.ri94@outlook.com>
2017-04-11 19:11:56 +0200

    Fix camera problem

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -1,10 +1,7 @@
 class CameraController < ApplicationController
 
   def photo
-    system "fswebcam -p #{ENV["CAMERA_PALETE"]}
-                     -r #{ENV["CAMERA_RESOLUTION"]}
-                     --no-banner
-                     #{ENV["CAMERA_DIR"]}"
+    system "fswebcam -p #{ENV["CAMERA_PALETE"]} -r #{ENV["CAMERA_RESOLUTION"]} --no-banner #{ENV["CAMERA_DIR"]}"
     send_file ENV["CAMERA_DIR"], type: 'image/jpg', disposition: 'inline'
   end
 
