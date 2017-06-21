013da7991d59089baa2e38b2cd10cb48fbcbd8ef
javierrodriguez94 <javier.ri94@outlook.com>
2017-06-22 00:59:17 +0200

    add token validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  before_action :validate_token
+
+  def validate_token
+    unless User.where(token: 1).any?#params[:token]).any?
+      head(403)
+    end
+  end
+
 end
