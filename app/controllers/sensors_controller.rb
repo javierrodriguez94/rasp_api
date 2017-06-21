df5f3c7338529691ba7147f30697e505ee3ed687
javierrodriguez94 <javier.ri94@outlook.com>
2017-06-22 00:31:10 +0200

    Fix sensors

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -1,12 +1,13 @@
 class SensorsController < ApplicationController
 
   require "dht-sensor-ffi"
-
+  pin = ENV["DHT_PIN"].to_i
+  type = ENV["DTH_TYPE"].to_i
   def temp
-    render json: DhtSensor.read(ENV["DHT_PIN"].to_i, ENV["DTH_TYPE"].to_i).temp
+    render json: DhtSensor.read(pin, type ).temp
   end
 
   def humidity
-    render json: DhtSensor.read(ENV["DHT_PIN"].to_i, ENV["DTH_TYPE"].to_i).humidity
+    render json: DhtSensor.read(pin, type).humidity
   end
 end
